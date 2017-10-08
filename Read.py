f7838e9b6f990e1f87bd616c93bf4cb2a67f44d1
kafey <febri@saklik.com>
2017-10-08 13:46:53 +0700

    Update Read.py

--- Read.py
+++ Read.py
@@ -42,7 +42,7 @@ for k, v in dir_sis.iteritems():
     # If we have the UID, continue
         if status == MIFAREReader.MI_OK:
             if uid == v:
-            print k + " sudah absen"
+                print k + " sudah absen"
 
         # Print UID
             print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
